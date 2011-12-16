c575fdd6f18fe4d53b597b13527d5c467a01e843
Jonathan Rico <jonathan_rico@softwarenextdoor.com>
2011-12-15 16:19:41 -0800

    Update lib/omniauth/strategies/salesforce.rb

--- lib/omniauth/strategies/salesforce.rb
+++ lib/omniauth/strategies/salesforce.rb
@@ -31,7 +31,7 @@ module OmniAuth
         end
         if options.has_key?(:customurl)
           puts '>>>> includes custom url'
-          default_options[:client_options][:site] = options[:customurl]
+          options[:site] = options[:customurl]
         end
         super
       end
