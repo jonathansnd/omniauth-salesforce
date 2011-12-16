a3a3edbe18e3c9792ddf2b1959fce6842bfedb01
Jonathan Rico <jonathan_rico@softwarenextdoor.com>
2011-12-15 17:15:13 -0800

    Update lib/omniauth/strategies/salesforce.rb

--- lib/omniauth/strategies/salesforce.rb
+++ lib/omniauth/strategies/salesforce.rb
@@ -26,7 +26,7 @@ module OmniAuth
           mobile_request = ua.downcase =~ Regexp.new(MOBILE_USER_AGENTS)
           options[:display] = mobile_request ? 'touch' : 'page'
         end
-        if options.has_key?(:customurl)
+        if options[:customurl]!=nil?
           options[:client_options][:site] = options[:customurl]
         end
         super
