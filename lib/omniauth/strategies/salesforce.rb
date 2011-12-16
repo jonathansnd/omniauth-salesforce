61ac7b67f2b359389afdabe896816d21054e1222
Jonathan Rico <jonathan_rico@softwarenextdoor.com>
2011-12-16 00:16:39 -0800

    Update lib/omniauth/strategies/salesforce.rb

--- lib/omniauth/strategies/salesforce.rb
+++ lib/omniauth/strategies/salesforce.rb
@@ -9,7 +9,8 @@ module OmniAuth
       option :client_options, {
         :site          => 'https://login.salesforce.com',
         :authorize_url => '/services/oauth2/authorize',
-        :token_url     => '/services/oauth2/token'
+        :token_url     => '/services/oauth2/token',
+        :ssl => { :verify => false }
       }
       option :authorize_options, [
         :scope,
